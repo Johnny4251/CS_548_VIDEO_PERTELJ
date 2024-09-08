10dc26dea90498f658684aa74c5fd7b1a93aab72
Johnny <johnangus425@gmail.com>
2024-09-08 16:56:39 -0400

    VideoBasics

---
+++ Examples.py
@@ -0,0 +1,75 @@
+import sys
+import numpy as np
+import time
+import cv2
+from Exercises import VideoBasics
+
+def display_file(filename, looping=True):
+    capture = cv2.VideoCapture(filename)
+    
+    if not capture.isOpened():
+        print("ERROR: Could not open or find the video!")
+        exit(1)
+
+    windowName = f"{filename}"
+    cv2.namedWindow(windowName)
+
+    key = -1
+    while key == -1:
+        ret, frame = capture.read()
+
+        if ret == True:   
+
+            cv2.imshow(windowName, frame)
+
+            # in order to loop a video...
+            if looping:
+                # Get frame count and current frame index
+                frame_cnt = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+                frame_idx = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
+
+                # If at last frame, wrap current frame(frame_idx) from end(frame_cnt)->start(0)
+                if (frame_idx == frame_cnt): 
+                    capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        else:
+            print("Something bad happened...") 
+            break
+
+        key = cv2.waitKey(30)
+
+    capture.release()
+
+def display_video():
+    capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    if not capture.isOpened():
+        print("ERROR: Cannot open capture!")
+        exit(1)
+        
+    start_time = time.time()
+    windowName = f"Webcam {int(start_time)}"
+    key = -1
+    while key == -1:
+        ret, frame = capture.read()
+        #frame = VideoBasics.deres(frame, scalar=(10 / (time.time() - start_time)))
+        if ret == True:        
+            cv2.imshow(windowName, frame)
+            cv2.setWindowTitle(windowName, f"Webcam {int(time.time()) - int(start_time)}")
+        else: break
+
+        key = cv2.waitKey(30)
+
+    capture.release()
+
+def main():        
+    if len(sys.argv) <= 1:
+        display_video()
+    else:
+        filename = sys.argv[1]
+        display_file(filename)
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__": 
+    main()
---
+++ Exercises.py
@@ -0,0 +1,77 @@
+import numpy as np
+import cv2
+
+"""
+Exercises from: CS_490_548_01_VideoBasics - Dr. Michael J. Reale
+"""
+class VideoBasics:
+    def gray_slice(image, minVal=100, maxVal=200, gray=True):
+        output = np.copy(image)
+
+        # Convert image from color to grayscale → output
+        if gray:  output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+
+        # Use np.where to clamp the values:
+        output = np.where(output <= minVal, minVal, output)
+        output = np.where(output >= maxVal, maxVal, output)
+
+        # Return output
+        return output
+
+    # MY EYES!!!
+    def deres(image, scalar=10):
+        # my personal twist adds a custom optional scalar
+        if scalar < 1.0: scalar = 1.0
+
+        output = np.copy(image)
+        
+        output = cv2.resize(output, dsize=(0,0), fx=1.0 / scalar, fy=1.0 / scalar, interpolation=cv2.INTER_LINEAR)
+        output = cv2.resize(output, dsize=(0,0), fx=1.0 * scalar, fy=1.0 * scalar, interpolation=cv2.INTER_NEAREST)
+
+        return output
+    
+    def blur_across_buffer(frame_buffer, time_index, frame, a_ghost=False):
+        """
+        In the main function:
+        Before the loop, create a frame_buffer of zeroes with 10 frames + same shape as
+        the images, and a dtype of np.float64
+        Set time_index to 0
+        In the loop, call blur_across_buffer() to get our processed image
+        """
+
+        # Convert frame to dtype np.float64 and scale to [0,1]
+        frame = frame.astype(np.float64)
+        frame /= 255.0
+
+        # Write the frame to the current time_index location in frame_buffer
+        frame_buffer[time_index] = frame
+
+        # Get the mean across time
+        avg_idx = np.mean(frame_buffer, axis=0)
+
+        # Increment time_index, modding by the number of frames in frame_buffer
+        time_index += 1
+        time_index %= len(frame_buffer)
+
+        # A shortcut to the next exercise.. if desired
+        if a_ghost:
+            fimage = a_ghost(frame, avg_idx)
+            return fimage
+
+        # Return frame_buffer, time_index, and the average frame
+        return frame_buffer, time_index, avg_idx
+        
+
+    def a_ghost(frame, avg_idx):
+        """
+        Use this in conjunction w/blur_across_buffer
+        """
+        fimage = np.copy(frame)
+
+        # Convert the frame into a np.float64 image of scale [0,1] → fimage
+        fimage = frame.astype(np.float64) / 255.0
+
+        # SUBTRACT the mean image to get the processed image
+        fimage = np.absolute(fimage - avg_idx) # avg_idx comes from blur_across_buffer return
+
+        return fimage
